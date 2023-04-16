87f0d44c83ddfc19a00b8faa69cc3d618869f364
Karol Przybylak <karolprzybylak2@gmail.com>
2023-04-16 16:07:55 +0200

    All working and optimalized endpoints

--- Controllers/OtomotoController.cs
+++ Controllers/OtomotoController.cs
@@ -1,5 +1,6 @@
 ﻿using Azure.Core;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using OtomotoSimpleBackend.Entities;
 
 namespace OtomotoSimpleBackend.Controllers
@@ -18,37 +19,74 @@ namespace OtomotoSimpleBackend.Controllers
         [HttpGet("GetOffers")]
         public IActionResult GetOffers()
         {
-            var offers = _context.Offers.ToList(); // Pobranie wszystkich ofert z bazy danych
+            var offers = _context.Offers
+                .AsNoTracking()
+                .Select(o => new
+                {
+                    o.Id,
+                    o.Brand,
+                    o.Model,
+                    o.EngineSizeInL,
+                    o.ProductionYear,
+                    o.Milleage,
+                    o.CreatedDate,
+                    o.OwnerId
+                })
+                .ToList();
 
-            return Ok(offers); // Zwrócenie ofert jako odpowiedź HTTP 200 OK
+            return Ok(offers);
         }
 
         [HttpGet("GetOwners")]
         public IActionResult GetOwners()
         {
-            var owners = _context.Owners.ToList(); // Pobranie wszystkich ofert z bazy danych
+            var owners = _context.Owners
+                .AsNoTracking()
+                .Include(o => o.Offers)
+                .ToList();
 
-            return Ok(owners); // Zwrócenie ofert jako odpowiedź HTTP 200 OK
+            var result = owners.Select(owner => new
+            {
+                owner.Id,
+                owner.FirstName,
+                owner.LastName,
+                offersCount = owner.Offers.Count
+            });
+
+            return Ok(result);
         }
 
         [HttpGet("GetOwnerOffers/{ownerId}")]
         public IActionResult GetOwnerOffers(Guid ownerId)
         {
             var offers = _context.Offers
+                .AsNoTracking()
                 .Where(o => o.OwnerId == ownerId)
-                .ToList(); // Pobranie wszystkich ofert z bazy danych
+                .Select(o => new
+                {
+                    o.Id,
+                    o.Brand,
+                    o.Model,
+                    o.EngineSizeInL,
+                    o.ProductionYear,
+                    o.Milleage,
+                    o.CreatedDate,
+                    o.OwnerId
+                })
+                .ToList();
 
-            return Ok(offers); // Zwrócenie ofert jako odpowiedź HTTP 200 OK
+            return Ok(offers);
         }
 
         [HttpGet("GetOfferById/{offerId}")]
         public IActionResult GetOfferById(Guid offerId)
         {
             var offer = _context.Offers
+                .AsNoTracking()
                 .Where(o => o.Id == offerId)
-                .ToList(); // Pobranie wszystkich ofert z bazy danych
+                .ToList();
 
-            return Ok(offer); // Zwrócenie ofert jako odpowiedź HTTP 200 OK
+            return Ok(offer);
         }
 
         [HttpPost("CreateOffer")]
@@ -60,7 +98,6 @@ namespace OtomotoSimpleBackend.Controllers
                 return NotFound("Owner doesn't exist");
             }
 
-            // Tworzenie nowej oferty
             var offer = new Offer
             {
                 Brand = request.Brand,
@@ -68,49 +105,43 @@ namespace OtomotoSimpleBackend.Controllers
                 EngineSizeInL = request.EngineSizeInL,
                 ProductionYear = request.ProductionYear,
                 Milleage = request.Milleage,
-                Owner = owner, // Przypisanie istniejącego właściciela do oferty
+                Owner = owner,
             };
 
-            // Zapisanie nowej oferty do bazy danych
             _context.Offers.Add(offer);
             _context.SaveChanges();
 
-            // Zwrócenie odpowiedzi HTTP
             return Ok(offer);
         }
 
         [HttpPost("CreateOwner")]
         public IActionResult CreateOwner(CreateOwnerRequest request)
         {
-            // Tworzenie nowego właściciela
             var owner = new Owner
             {
                 FirstName = request.FirstName,
                 LastName = request.LastName,
                 PhoneNumber = request.PhoneNumber,
                 City = request.City,
-                Offers = new List<Offer>()  // Przypisanie pustej listy ofert
+                Offers = new List<Offer>()
             };
 
-            // Zapisanie nowego właściciela do bazy danych
             _context.Owners.Add(owner);
             _context.SaveChanges();
 
-            // Zwrócenie odpowiedzi HTTP
             return Ok(owner);
         }
 
         [HttpPut("PutOffer/{id}")]
         public IActionResult PutOffer(Guid id, [FromBody] CreateOfferRequest updatedOfferRequest)
         {
-            var existingOffer = _context.Offers.FirstOrDefault(o => o.Id == id); // Pobranie istniejącej oferty z bazy danych
+            var existingOffer = _context.Offers.FirstOrDefault(o => o.Id == id);
 
             if (existingOffer == null)
             {
                 return NotFound("Offer doesn't exist");
             }
 
-            // Zaktualizuj dane oferty na podstawie przesłanych informacji
             existingOffer.Brand = updatedOfferRequest.Brand;
             existingOffer.Model = updatedOfferRequest.Model;
             existingOffer.EngineSizeInL = updatedOfferRequest.EngineSizeInL;
@@ -118,45 +149,43 @@ namespace OtomotoSimpleBackend.Controllers
             existingOffer.Milleage = updatedOfferRequest.Milleage;
             existingOffer.OwnerId = updatedOfferRequest.OwnerId;
 
-            _context.SaveChanges(); // Zapisz zmiany w bazie danych
+            _context.SaveChanges();
 
-            return Ok(existingOffer); // Zwróć zaktualizowaną ofertę jako odpowiedź HTTP 200 OK
+            return Ok(existingOffer);
         }
 
         [HttpPut("PutOwner/{id}")]
         public IActionResult PutOwner(Guid id, [FromBody] CreateOwnerRequest updatedOwnerRequest)
         {
-            var existingOwner = _context.Owners.FirstOrDefault(o => o.Id == id); // Pobranie istniejącej oferty z bazy danych
+            var existingOwner = _context.Owners.FirstOrDefault(o => o.Id == id);
 
             if (existingOwner == null)
             {
                 return NotFound("Owner doesn't exist");
             }
 
-            // Zaktualizuj dane oferty na podstawie przesłanych informacji
             existingOwner.FirstName = updatedOwnerRequest.FirstName;
             existingOwner.LastName = updatedOwnerRequest.LastName;
             existingOwner.PhoneNumber = updatedOwnerRequest.PhoneNumber;
             existingOwner.City = updatedOwnerRequest.City;
 
-            _context.SaveChanges(); // Zapisz zmiany w bazie danych
+            _context.SaveChanges();
 
-            return Ok(existingOwner); // Zwróć zaktualizowaną ofertę jako odpowiedź HTTP 200 OK
+            return Ok(existingOwner);
         }
 
         [HttpDelete("DeleteOffer{id}")]
         public IActionResult DeleteOffer(Guid id)
         {
-            // Znajdź ofertę do usunięcia na podstawie podanego ID
             var offer = _context.Offers.FirstOrDefault(o => o.Id == id);
 
             if (offer == null)
             {
-                return NotFound("Offer doesn't exist"); // Jeśli oferta nie istnieje, zwróć odpowiedź HTTP 404 Not Found
+                return NotFound("Offer doesn't exist");
             }
 
-            _context.Offers.Remove(offer); // Usuń ofertę z kontekstu bazy danych
-            _context.SaveChanges(); // Zapisz zmiany w bazie danych
+            _context.Offers.Remove(offer);
+            _context.SaveChanges();
 
             return Ok(offer);
         }
@@ -164,19 +193,17 @@ namespace OtomotoSimpleBackend.Controllers
         [HttpDelete("DeleteOwner{id}")]
         public IActionResult DeleteOwner(Guid id)
         {
-            // Znajdź ofertę do usunięcia na podstawie podanego ID
             var owner = _context.Owners.FirstOrDefault(o => o.Id == id);
 
             if (owner == null)
             {
-                return NotFound("Offer doesn't exist"); // Jeśli oferta nie istnieje, zwróć odpowiedź HTTP 404 Not Found
+                return NotFound("Offer doesn't exist");
             }
 
-            _context.Owners.Remove(owner); // Usuń ofertę z kontekstu bazy danych
-            _context.SaveChanges(); // Zapisz zmiany w bazie danych
+            _context.Owners.Remove(owner);
+            _context.SaveChanges();
 
             return Ok(owner);
         }
-
     }
 }
