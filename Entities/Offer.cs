8883e6dceb493243c5e3d88dbff59d72519a590c
Karol Przybylak <karolprzybylak2@gmail.com>
2023-04-18 13:35:57 +0200

    Add porops to models and migrate it

--- Entities/Offer.cs
+++ Entities/Offer.cs
@@ -1,11 +1,19 @@
-﻿namespace OtomotoSimpleBackend.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OtomotoSimpleBackend.Entities
 {
     public class Offer
     {
         public Guid Id { get; set; } = Guid.NewGuid();
         public string Brand { get; set; }
         public string Model { get; set; }
+        public string Body { get; set; }
+        public string Description { get; set; }
+        public int PriceInEur { get; set; }
+        public string FuelType { get; set; }
         public double EngineSizeInL { get; set; }
+        public int HorsePower { get; set; }
+        public bool AutomaticTransmission { get; set; }
         public int ProductionYear { get; set; }
         public int Milleage { get; set; }
         public DateTime CreatedDate { get; set; } = DateTime.Now;
--- Entities/Owner.cs
+++ Entities/Owner.cs
@@ -7,6 +7,8 @@
         public string LastName { get; set; }
         public int PhoneNumber { get; set; }
         public string City { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
 
         public List<Offer> Offers { get; set; }
     }
