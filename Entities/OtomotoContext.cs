aa9cd38a7afc5ed48e36a1ddb4e982f9b1710f27
Karol Przybylak <karolprzybylak2@gmail.com>
2023-04-19 18:39:06 +0200

    Configure Offer Table

--- Entities/OtomotoContext.cs
+++ Entities/OtomotoContext.cs
@@ -14,16 +14,36 @@ namespace OtomotoSimpleBackend.Entities
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<Offer>()
-                .HasKey(o => o.Id);
-
-            modelBuilder.Entity<Owner>()
-                .HasKey(o => o.Id);
-
-            modelBuilder.Entity<Offer>()
-                .HasOne(o => o.Owner)
+            modelBuilder.Entity<Offer>(eb =>
+            {
+                eb.HasKey(o => o.Id);
+                eb.HasOne(o => o.Owner)
                 .WithMany(o => o.Offers)
                 .HasForeignKey(o => o.OwnerId);
+
+                eb.Property(o => o.Brand)
+                .IsRequired()
+                .HasMaxLength(20);
+
+                eb.Property(o => o.Model)
+                .IsRequired()
+                .HasMaxLength(20);
+
+                eb.Property(o => o.Body)
+                .HasMaxLength(20);
+
+                eb.Property(o => o.FuelType)
+                .HasMaxLength(15);
+
+                eb.Property(o => o.EngineSizeInL)
+                .HasPrecision(5, 3);
+            });
+
+            modelBuilder.Entity<Owner>(eb =>
+            {
+                eb.HasKey(o => o.Id);
+
+            });
 
             base.OnModelCreating(modelBuilder);
         }
