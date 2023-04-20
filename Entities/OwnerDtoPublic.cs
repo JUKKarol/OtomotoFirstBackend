c5189a14c8c787c24f8b3095723541d48b3aade9
Karol Przybylak <karolprzybylak2@gmail.com>
2023-04-20 17:00:56 +0200

    Add all dto models

--- Entities/OwnerDtoPublic.cs
+++ Entities/OwnerDtoPublic.cs
@@ -1,11 +1,10 @@
 ﻿namespace OtomotoSimpleBackend.Entities
 {
-    public class OwnerDto
+    public class OwnerDtoPublic
     {
-        public Guid Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        public int PhoneNumber { get; set; }
         public string City { get; set; }
-        public DateTime CreatedDate { get; set; }
     }
 }
---
+++ Entities/OwnerDtoRegistration.cs
@@ -0,0 +1,12 @@
+﻿namespace OtomotoSimpleBackend.Entities
+{
+    public class OwnerDtoRegistration
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int PhoneNumber { get; set; }
+        public string City { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
