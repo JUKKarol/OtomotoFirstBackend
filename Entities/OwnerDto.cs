ecab8e3e340a78abd8d28b494527f4ca73bdf233
Karol Przybylak <karolprzybylak2@gmail.com>
2023-04-20 16:18:03 +0200

    Add Dto

---
+++ Entities/OwnerDto.cs
@@ -0,0 +1,11 @@
+﻿namespace OtomotoSimpleBackend.Entities
+{
+    public class OwnerDto
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string City { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}
