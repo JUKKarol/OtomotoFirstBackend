c1549f14c02e6736efd44b0b5a8801c80d9d3c03
Karol Przybylak <karolprzybylak2@gmail.com>
2023-05-03 20:52:42 +0200

    Split owner controller

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,121 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using OtomotoSimpleBackend.Data;
+using OtomotoSimpleBackend.DTOs;
+using OtomotoSimpleBackend.Entities;
+using OtomotoSimpleBackend.Services;
+
+namespace OtomotoSimpleBackend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly OtomotoContext _context;
+        private readonly IMapper _mapper;
+        private readonly IOwnerService _ownerService;
+
+        public AuthController(OtomotoContext context, IMapper mapper, IOwnerService ownerService)
+        {
+            _context = context;
+            _mapper = mapper;
+            _ownerService = ownerService;
+        }
+
+        [HttpPost("RegisterOwner")]
+        public async Task<IActionResult> RegisterOwner(OwnerDtoRegistration ownerDto)
+        {
+            if (_context.Owners.Any(u => u.Email == ownerDto.Email))
+            {
+                return BadRequest("User already exists");
+            }
+
+            _ownerService.CreatePasswordHash(ownerDto.Password,
+                out byte[] passwordHash
+            , out byte[] passwordSalt);
+
+            var owner = _mapper.Map<Owner>(ownerDto);
+
+            await _context.Owners.AddAsync(owner);
+            await _context.SaveChangesAsync();
+
+            return Ok("User successfully created");
+        }
+
+        [HttpPost("LoginOwner")]
+        public async Task<IActionResult> LoginOwner(OwnerDtoLogin ownerDto)
+        {
+            var owner = await _context.Owners.FirstOrDefaultAsync(o => o.Email == ownerDto.Email);
+
+            if (owner == null)
+            {
+                return BadRequest("User not found");
+            }
+
+            if (!_ownerService.VerifyPasswordHash(ownerDto.Password, owner.PasswordHash, owner.PasswordSalt))
+            {
+                return BadRequest("Password incorect");
+            }
+
+            if (owner.VerifiedAt == null)
+            {
+                return BadRequest("Not verified");
+            }
+
+            return Ok("Logged in");
+        }
+
+        [HttpPost("VerifyOwner")]
+        public async Task<IActionResult> VerifyOwner(string token)
+        {
+            var owner = await _context.Owners.FirstOrDefaultAsync(u => u.VeryficationToken == token);
+            if (owner == null)
+            {
+                return BadRequest("Invalid token");
+            }
+
+            owner.VerifiedAt = DateTime.Now;
+            await _context.SaveChangesAsync();
+
+            return Ok("User verified");
+        }
+
+        [HttpPost("ForgotOwnerPassword")]
+        public async Task<IActionResult> ForgotPassword(string email)
+        {
+            var owner = await _context.Owners.FirstOrDefaultAsync(u => u.Email == email);
+            if (owner == null)
+            {
+                return BadRequest("User not found");
+            }
+
+            owner.PasswordResetToken = _ownerService.CreateRandomToken();
+            owner.ResetTokenExpires = DateTime.Now.AddDays(1);
+            await _context.SaveChangesAsync();
+
+            return Ok("You may now reset your password");
+        }
+
+        [HttpPost("ResetOwnerPassword")]
+        public async Task<IActionResult> ResettPassword(OwnerDtoResetPassword ownerDto)
+        {
+            var owner = await _context.Owners.FirstOrDefaultAsync(u => u.PasswordResetToken == ownerDto.Token);
+            if (owner == null || owner.ResetTokenExpires < DateTime.Now)
+            {
+                return BadRequest("Invalid Token.");
+            }
+
+            _ownerService.CreatePasswordHash(ownerDto.Password, out byte[] passwordHash, out byte[] passwordSalt);
+
+            owner.PasswordHash = passwordHash;
+            owner.PasswordSalt = passwordSalt;
+            owner.PasswordResetToken = null;
+            owner.ResetTokenExpires = null;
+
+            await _context.SaveChangesAsync();
+
+            return Ok("Password successfully reset.");
+        }
+    }
+}
